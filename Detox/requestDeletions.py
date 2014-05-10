219eb08715df8d6e14ddb24fa147606bcef83a45
Christoph Paus <paus@mit.edu>
2014-05-09 20:17:20 -0400

    Upate sites.

--- Detox/requestDeletions.py
+++ Detox/requestDeletions.py
@@ -120,24 +120,30 @@ statusDirectory = os.environ['DETOX_DB'] + '/' + os.environ['DETOX_STATUS']
 resultDirectory = os.environ['DETOX_DB'] + '/' + os.environ['DETOX_RESULT']
 
 # define test sites
-testSites = [ 'T2_AT_Vienna',
-              'T2_BR_SPRACE',
-              'T2_BE_IIHE','T2_BE_UCL'
-              'T2_CH_CSCS',
-              'T2_CN_Beijing',
-              'T2_DE_DESY','T2_DE_RWTH',
-              'T2_EE_Estonia',
-              'T2_ES_CIEMAT','T2_ES_IFCA',
-              'T2_FI_HIP',
-              'T2_FR_IPHC','T2_FR_GRIF_LLR','T2_FR_CCIN2P3','T2_FR_GRIF_IRFU',
-              'T2_IN_TIFR',
-              'T2_IT_Pisa','T2_IT_Bari','T2_IT_Legnaro','T2_IT_Rome',
-              'T2_KR_KNU',
-              'T2_RU_JINR','T2_RU_IHEP',
-              'T2_UA_KIPT',
-              'T2_UK_London_IC','T2_UK_London_Brunel',
-              'T2_US_Caltech','T2_US_Florida','T2_US_MIT','T2_US_Nebraska','T2_US_Purdue',
-              'T2_US_Wisconsin','T2_US_UCSD'
+testSites = [ 'T2_AT_Vienna'
+              ,'T2_BE_IIHE','T2_BE_UCL'
+              ,'T2_BR_SPRACE','T2_BR_UERJ'
+              ,'T2_CH_CSCS'
+              ,'T2_CN_Beijing'
+              ,'T2_DE_DESY','T2_DE_RWTH'
+              ,'T2_EE_Estonia'
+              ,'T2_ES_CIEMAT','T2_ES_IFCA'
+              ,'T2_FI_HIP'
+              ,'T2_FR_CCIN2P3','T2_FR_GRIF_IRFU','T2_FR_GRIF_LLR','T2_FR_IPHC'
+              ,'T2_GR_Ioannina'
+              ,'T2_HU_Budapest'
+              ,'T2_IN_TIFR'
+              ,'T2_IT_Bari','T2_IT_Legnaro','T2_IT_Pisa','T2_IT_Rome'
+              ,'T2_KR_KNU'
+              ,'T2_PL_Warsaw'
+              ,'T2_PT_NCG_Lisbon'
+              ,'T2_RU_IHEP','T2_RU_INR','T2_RU_ITEP','T2_RU_JINR','T2_RU_SINP'
+              ,'T2_TR_METU'
+              ,'T2_TW_Taiwan'
+              ,'T2_UA_KIPT'
+              ,'T2_UK_London_Brunel','T2_UK_London_IC','T2_UK_SGrid_RALPP'
+              ,'T2_US_Caltech','T2_US_Florida','T2_US_MIT','T2_US_Nebraska','T2_US_Purdue'
+              ,'T2_US_UCSD','T2_US_Vanderbilt','T2_US_Wisconsin'
               ]
 
 # record active sites in a file
