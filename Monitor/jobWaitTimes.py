8d7595d346cc42bd162dc96118d2368fb7f0dd8a
Siddharth <sidn@mit.edu>
2014-11-10 11:40:13 -0500

    fixed sizes in findDatasetProperties updated nAccess plots to be more accurate

--- Monitor/jobWaitTimes.py
+++ Monitor/jobWaitTimes.py
@@ -9,17 +9,35 @@ def epochTime(s):
 def processFile(fileName,debug=0):
   # process the contents of a simple file into a dict of dicts:
   # waitTimes[dataset][site] is the mean wait time of jobs on 'site' looking for 'dataset'
-  print fileName
+  # print fileName
   data_file = open(fileName,'r')
   waitTimes={}
+  maxTuple = (0,None,None)
   nJobs={}
+  returnList=[]
   for line in data_file.readlines():
     vals=line.split()
+    jID = int(vals[0])
     datasetName = vals[1]
     siteName = vals[2]
+    if not re.search(r'/.*/.*/.*AOD.*',datasetName):
+      #not AOD or AODSIM
+      continue
+    if not re.search(r'T2.*',siteName):
+      # not on a tier 2
+      continue
     start = int(vals[3])
     submitted = int(vals[4])
     wait = max(start - submitted,0)
+    if wait > 10**7:
+      sys.stderr.write("%s\n"%(str(vals)))
+      continue
+    if maxTuple[0] < wait:
+      maxTuple = (wait,datasetName,siteName)
+    returnList.append(wait)
+    # maxWait = max(maxWait,wait)
+    # if wait > 86400:
+    #   print "Warning, job %i waited for %i = %i - %i\n\t%s\n\t%s\n"%(jID, wait, start, submitted, datasetName, siteName)
     if not datasetName in waitTimes:
       waitTimes[datasetName]={}
       nJobs[datasetName]={}
@@ -31,17 +49,17 @@ def processFile(fileName,debug=0):
     else:
       waitTimes[datasetName][siteName] = wait
       nJobs[datasetName][siteName] = 1
-  return waitTimes, None
+  return waitTimes, returnList, maxTuple
 
 def processFileJson(fileName,debug=0):
   # process the contents of a simple json file into a dict of dicts:
   # waitTimes[dataset][site] is the mean wait time of jobs on 'site' looking for 'dataset'
   # also returns list of lists with structure:
   # [jobID, datasetName,SiteName, started, submitted, finished, infoTimeStamp]
-  print fileName
+  # print fileName
   with open(fileName) as data_file:
     data = json.load(data_file)
-
+  maxTuple = (0,None,None)
   waitTimes={}
   nJobs={}
   jobs = data["jobs"]
@@ -52,6 +70,9 @@ def processFileJson(fileName,debug=0):
       #not AOD or AODSIM
       continue
     siteName = job["SiteName"]
+    if not re.search(r'T2.*',siteName):
+      # not on a tier 2
+      continue
     start = epochTime(job["StartedRunningTimeStamp"])
     finish = epochTime(job["FinishedTimeStamp"])
     submitted = epochTime(job["SubmittedTimeStamp"])
@@ -59,6 +80,12 @@ def processFileJson(fileName,debug=0):
     jID = job["JobId"]
     returnList.append([jID, datasetName, siteName, start, submitted, finish, latestInfo])
     wait = max(start - submitted,0)
+    if wait > 10**7:
+      print returnList[-1]
+    if maxTuple[0] < wait:
+      maxTuple = (wait,datasetName,siteName)
+    # if wait > 86400:
+      # print "Warning, job %i waited for %i = %i - %i\n\t%s\n\t%s"%(jID, wait, start, submitted, datasetName, siteName)
     if not datasetName in waitTimes:
       waitTimes[datasetName]={}
       nJobs[datasetName]={}
@@ -71,19 +98,23 @@ def processFileJson(fileName,debug=0):
       waitTimes[datasetName][siteName] = wait
       nJobs[datasetName][siteName] = 1
 
-  return waitTimes, returnList
+  return waitTimes, returnList, maxTuple
+
+'''
+#### MAIN ####
+'''
 
 timeStart=None
 timeEnd=None
 
 if len(sys.argv) == 1:
     print "Using default times"
-    print "If desired, enter start end times in format YYYY-MM-DD%hh:mm:ss"
+    print "If desired, enter start end epoch times"
     timeStart = list(time.gmtime(time.time()-600))[:6] 
     timeEnd = list(time.gmtime())[:6] 
 else:
-    timeStart = re.split('[-%:]',sys.argv[1])
-    timeEnd = re.split('[-%:]',sys.argv[2])
+    timeStart = time.gmtime(int(sys.argv[1]))
+    timeEnd = time.gmtime(int(sys.argv[2]))
 
 # timeStart = [2014, 10, 13, 00, 00, 00] # testing
 # timeEnd = [2014, 10, 13, 00, 59, 00]
@@ -106,8 +137,9 @@ outFilePath = os.environ.get('MONITOR_DB') + "/waitTimes/%i_%i.txt"%(timeStartSt
 logFilePath = os.environ.get('MONITOR_DB') + "/waitTimes/%i_%i.log"%(timeStartString,timeEndString)
 
 pid = os.getpid()
-if os.path.isfile(outFilePath):
-    waitTimes, compressedList = processFile(outFilePath)
+jsonExists = os.path.isfile(outFilePath)
+if jsonExists:
+    waitTimes, compressedList, maxWait = processFile(outFilePath)
 else:
     os.system("mkdir -p %s/waitTimes"%(os.environ.get("MONITOR_DB")))
     curlPath = 'http://dashb-cms-datapop.cern.ch/dashboard/request.py/cms-wait-time-api?start=%.2i-%.2i-%.2i@20%.2i:%.2i:%.2i&end=%.2i-%.2i-%.2i@20%.2i:%.2i:%.2i'%(timeStart[0],
@@ -123,24 +155,33 @@ else:
       timeEnd[4],
       timeEnd[5])
     curlPath = curlPath.replace("@","%")
-    print "curl -H 'Accept: application/json' '%s' "%(curlPath)
+    # print "curl -H 'Accept: application/json' '%s' "%(curlPath)
     os.system("mkdir -p tmp")
     os.system("curl -H 'Accept: application/json' '%s' > tmp/%i.json"%(curlPath,pid))
     # os.system("curl -H 'Accept: application/json' '%s'| python -mjson.tool > tmp/%i.json"%(curlPath,pid))
-    waitTimes, compressedList = processFileJson("tmp/%i.json"%(pid))
-    os.system("rm tmp/%i.json"%(pid))
+    waitTimes, compressedList, maxWait = processFileJson("tmp/%i.json"%(pid))
+    # os.system("rm tmp/%i.json"%(pid))
 
 formatString = ["%i ", "%s ", "%s ", "%i ", "%i ", "%i ", "%i "]
+
+# sys.stdout.write(str(maxWait[0])+"\n")
 # print compressedList
-if compressedList:
-    outFile = open(outFilePath,'w')
-    for l in compressedList:
-      for i in xrange(7):
-        outFile.write(formatString[i]%(l[i]))
-      outFile.write("\n")
-    outFile.close()
+if not jsonExists:
+  outFile = open(outFilePath,'w')
+  for l in compressedList:
+    # sys.stdout.write("%.3f\n"%(max(l[3]-l[4],0)))
+    for i in xrange(len(formatString)):
+      outFile.write(formatString[i]%(l[i]))
+    outFile.write("\n")
+  outFile.close()
+else:
+  pass
+  # for l in compressedList:
+  #   sys.stdout.write("%.3f\n"%(l))
 
 logFile = open(logFilePath,'w')
+nTotal = 0
+totalWait = 0
 for d in waitTimes:
   logFile.write(d+"\n")
   for s in waitTimes[d]:
@@ -148,5 +189,7 @@ for d in waitTimes:
       continue
     if waitTimes[d][s]:
       logFile.write("\t%20s"%(s)+"\t%.3f\n"%(waitTimes[d][s]))
+    totalWait += waitTimes[d][s]
+    nTotal += 1
+sys.stdout.write("%.3f\n"%(float(totalWait)/nTotal))
 logFile.close()
-
--- Monitor/plotDatasets.C
+++ Monitor/plotDatasets.C
@@ -22,23 +22,24 @@ using namespace std;
 void plotDatasetUsage();
 
 //--------------------------------------------------------------------------------------------------
-void plotDatasets(int average=0)
+void plotDatasets(int type=0, int interval=1)
 {
   TString  styleMacro = gSystem->Getenv("MIT_ROOT_STYLE");
   long int rc = gROOT->LoadMacro(styleMacro+"+");
   printf(" Return code of loading styles: %d\n",rc);
 
-  plotDatasetUsage(average);
+  plotDatasetUsage(type,interval);
 }
 
 //--------------------------------------------------------------------------------------------------
-void plotDatasetUsage(int average=0)
+void plotDatasetUsage(int type=0, int interval=1)
 {
   TString text        = gSystem->Getenv("DATASET_MONITOR_TEXT");
   TString fileName    = gSystem->Getenv("DATASET_MONITOR_FILE");
 
   TString pngFileName = fileName + TString(".png");
-  pngFileName = fileName + TString(".png"); // if we're using nSitesAv instead of nSites
+  if(type==1) pngFileName = fileName + TString("_nSitesAverage.png"); // if we're using nSitesAv instead of nSites
+  else pngFileName = fileName + TString("_nSites.png"); // if we're using nSitesAv instead of nSites
   TString inputFile   = fileName + TString(".txt");
 
   // Make sure we have the right styles
@@ -50,7 +51,7 @@ void plotDatasetUsage(int average=0)
 
   Int_t    nLines=0;
   Double_t totalSize=0;
-  Double_t xMin=0, xMax=120;
+  Double_t xMin=0, xMax=120.;
   Double_t yMin=0, yMax=10;
   Int_t    nSites=0, nFiles=0, nAccesses=0;
   Double_t nSitesAv, size=0;
@@ -58,18 +59,21 @@ void plotDatasetUsage(int average=0)
 
   // book our histogram
   Int_t nBins = int(xMax-xMin);
-  if(average==2) nBins = 20;
+  if (interval!=1) xMax = xMax/14.; // divide by approximate number of months
   Int_t nBinsy = 20;
-  TH1D *h ; 
-  TH2D *h2 ;
-          if(average==2) h2 = new TH2D("dataUsage","Accesses vs TB",nBins,xMin,xMax,nBinsy,yMin,yMax);
-          else h = new TH1D("dataUsage","Data Usage",nBins,xMin-0.5,xMax+0.5);
-  if(average==2) MitRootStyle::InitHist(h2,"","",kBlack);
-  else MitRootStyle::InitHist(h,"","",kBlack);
-  TString titles = TString("; Accesses ") + text + TString(";Data Size [TB]");
-  if(average==2) h2->SetTitle(titles.Data());
-  else h->SetTitle(titles.Data());
-
+  TH1D *h;
+  TH1D *h2 ; // h is used for plotting, h2 is for computing some stuff
+  if (interval==1) h = new TH1D("dataUsage","Data Usage",nBins,xMin-0.5,xMax+0.5);
+  else h = new TH1D("dataUsage","Data Usage",nBins+1,xMin-(0.5+1)/14.,xMax+0.5/14.);
+  if (interval==1) h2 = new TH1D("dataUsage2","Data Usage",nBins,xMin-0.5,xMax+0.5);
+  else h2 = new TH1D("dataUsage2","Data Usage",nBins,xMin-(0.5)/14.,xMax+0.5/14.);
+  MitRootStyle::InitHist(h,"","",kBlack);
+  //TString titles = TString("; Accesses ") + text + TString(";Data Size [TB]");
+  //if(interval!=1) titles = TString("; Accesses/day ") + TString(";Data Size [TB]");
+  TString titles = TString("; Accesses ") + text + TString("; Fraction of total data volume");
+  if(interval!=1) titles = TString("; Accesses/month ") + TString("; Fraction of total data volume");
+  h->SetTitle(titles.Data());
+  if(interval!=1)  interval=interval/(86400*30);
   // Loop over the file
   //-------------------
   while (1) {
@@ -82,7 +86,7 @@ void plotDatasetUsage(int average=0)
     if (! input.good())
       break;
     
-    if (average==1) {
+    if (type==1) {
       // do we want nSites or nSitesAv
       // show what we are reading
       if (nLines < 5)
@@ -94,16 +98,11 @@ void plotDatasetUsage(int average=0)
     }
     Double_t value, valuey, weight;
     
-    if (average==1) {
-      value = double(nAccesses)/double(nFiles*nSitesAv); 
+    if (type==1) {
+      value = double(nAccesses)/double(nFiles*nSitesAv*interval); 
       weight = double(nSitesAv)*size/1024.;
-    } else if (average==2) {
-        value = double(nAccesses)/double(nFiles);
-        valuey = double(size/1024.);
-//        if (value) fprintf(stderr,"%i/(%i*%f) = %f\n",nAccesses,nFiles,size,value);
-        weight = double(nSitesAv)*size/1024.;
     } else {  
-      value = double(nAccesses)/double(nFiles*nSites); 
+      value = double(nAccesses)/double(nFiles*nSites*interval); 
       weight = double(nSites)*size/1024.;
     }
 
@@ -112,27 +111,25 @@ void plotDatasetUsage(int average=0)
     // - to avoid confusing counts below zero pretend that accessing half of the files of
     //   one replica counts as one full access
     //   not desired behavior when plotting nAccesses/GB, usually < 1
-    if ( value<1.0 && (double(nAccesses)/double(nFiles)>0.5))
-      value = 1.;
+    // if ( value<1.0 && (double(nAccesses)/double(nFiles)>0.5))
+    //   value = 1.;
 
     // truncate at maximum (overflow in last bin)
+
     if (value>=xMax)
       value = xMax-0.0001;
-    if(average==2) {
-       if(valuey>=yMax) value = yMax-0.0001;
-    } 
-
+    
     // fill the histogram here
-    if(average==2) h2->Fill(value,valuey,weight);
+    if (value==0) h->Fill(h->GetBinCenter(1),weight);
     else h->Fill(value,weight);
-
+    h2->Fill(value,weight);
     if (nLines < 5) {
-      if(average==2) printf(" Value: %f,%f weight: %f\n",value,valuey,weight);
+      if(type==2) printf(" Value: %f,%f weight: %f\n",value,valuey,weight);
       else printf(" Value: %f weight: %f\n",value,weight);
     }
 
     // keep track of the total size
-    if (average) totalSize += double(nSitesAv)*size/1024.;
+    if (type) totalSize += double(nSitesAv)*size/1024.;
     else totalSize += double(nSites)*size/1024.;
 
     // count the number of lines
@@ -147,16 +144,30 @@ void plotDatasetUsage(int average=0)
 
   // Open a canvas
   TCanvas *cv = new TCanvas();
- // if (average==2) cv->SetLogy();
+ // if (type==2) cv->SetLogy();
   cv->Draw();
-  if(average==2) {
-      h2->SetStats(0);
-      h2->Draw("colz");
-  }
-    else   h->Draw("hist");
+  Double_t integral=h->Integral();
+  h->Scale(1/integral);
+  h->SetMaximum(0.3); // for easy comparison between plots
+  h->Draw("hist");
 
   MitRootStyle::OverlayFrame();
-  MitRootStyle::AddText("Overflow in last bin.");
-
+  MitRootStyle::AddText("Overflow in last bin. nAccesses=0 in first bin.");
+  TString integralText = "Data managed: ";
+  char buffer[32];
+  sprintf(buffer,"%.3f PB",totalSize/1024.);
+  integralText+=buffer;
+  TText *plotText = new TText(.4,.75,integralText.Data());
+  plotText->SetTextSize(0.04);
+  plotText->Draw();
+  TText *plotText2 = new TText(.4,.8,text.Data());
+  plotText2->SetTextSize(0.04);
+  plotText2->Draw();
+  text = "Mean: ";
+  sprintf(buffer,"%.3f accesses/month",h2->GetMean(1));
+  text += buffer;
+  TText *plotText3 = new TText(.4,.7,text.Data());
+  plotText3->SetTextSize(0.04);
+  plotText3->Draw();
   cv->SaveAs(pngFileName.Data());
 }
