6e5a88f631173594702933d8d627944871725877
Bjorn Barrefors <barrefors@gmail.com>
2014-12-01 12:00:51 -0600

    Fixed indentation issues

--- DataDealer/src/datasetTrend.py
+++ DataDealer/src/datasetTrend.py
@@ -12,11 +12,11 @@ popDbApi_ = popDbApi.popDbApi()
 startDates = []
 dates = dict()
 for dataset in datasets:
-	jsonData = phedexApi_.data(dataset=dataset, level='block', create_since='0')
-	timestamp = jsonData.get('phedex').get('dbs')[0].get('dataset')[0].get('time_create')
-	startDate = datetime.datetime.fromtimestamp(timestamp)
-	dates[dataset] = startDate
-	startDates.append(startDate)
+    jsonData = phedexApi_.data(dataset=dataset, level='block', create_since='0')
+    timestamp = jsonData.get('phedex').get('dbs')[0].get('dataset')[0].get('time_create')
+    startDate = datetime.datetime.fromtimestamp(timestamp)
+    dates[dataset] = startDate
+    startDates.append(startDate)
 startDate = min(startDates)
 endDate = datetime.datetime.now()
 jsonDataCPU = popDbApi_.getDSNdata(tstart=startDate, tstop=endDate, aggr='week', orderby='totcpu')
--- DataDealer/src/systemData.py
+++ DataDealer/src/systemData.py
@@ -16,12 +16,12 @@ availableSites = sites_.getAvailableSites()
 today = datetime.date.today()
 date = today - datetime.timedelta(days=1)
 for siteName in availableSites:
-	query = "SELECT Quotas.SizeTb FROM Quotas INNER JOIN Sites ON Quotas.SiteId=Sites.SiteId INNER JOIN Groups ON Groups.GroupId=Quotas.GroupId WHERE Sites.SiteName=%s AND Groups.GroupName=%s"
+    query = "SELECT Quotas.SizeTb FROM Quotas INNER JOIN Sites ON Quotas.SiteId=Sites.SiteId INNER JOIN Groups ON Groups.GroupId=Quotas.GroupId WHERE Sites.SiteName=%s AND Groups.GroupName=%s"
     values = [siteName, "AnalysisOps"]
     data = dbApi.dbQuery(query, values=values)
     quota = data[0][0]*10**3
     used = phedexData_.getSiteStorage(siteName)
     cpu = popDbData_.getSiteCpu(siteName, date.strftime('%Y-%m-%d'))
-	fs = open('/local/cmsprod/IntelROCCS/DataDealer/Demo/DatasetData/System', 'a')
-	fs.write("%s\t%s\t%s\t%s\n" % (siteName, quota, used, cpu))
-	fs.close()
+    fs = open('/local/cmsprod/IntelROCCS/DataDealer/Demo/DatasetData/System', 'a')
+    fs.write("%s\t%s\t%s\t%s\n" % (siteName, quota, used, cpu))
+    fs.close()
