87fa8ef6fa99d399228da35f5539cb247100ad97
Bjorn Barrefors <barrefors@gmail.com>
2014-12-01 11:52:20 -0600

    Collecting data for system visualization

---
+++ DataDealer/src/systemData.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#---------------------------------------------------------------------------------------------------
+# Data modeling
+#---------------------------------------------------------------------------------------------------
+import sys, json, datetime
+import sites, dbApi, phedexData, popDbData
+
+fs = open('/local/cmsprod/IntelROCCS/DataDealer/Demo/DatasetData/System', 'w')
+fs.write("site\tquota\tused\tcpu\n")
+fs.close()
+dbApi_ = dbApi.dbApi()
+phedexData_ = phedexData.phedexData()
+popDbData_ = popDbData.popDbData()
+sites_ = sites.sites()
+availableSites = sites_.getAvailableSites()
+today = datetime.date.today()
+date = today - datetime.timedelta(days=1)
+for siteName in availableSites:
+	query = "SELECT Quotas.SizeTb FROM Quotas INNER JOIN Sites ON Quotas.SiteId=Sites.SiteId INNER JOIN Groups ON Groups.GroupId=Quotas.GroupId WHERE Sites.SiteName=%s AND Groups.GroupName=%s"
+    values = [siteName, "AnalysisOps"]
+    data = dbApi.dbQuery(query, values=values)
+    quota = data[0][0]*10**3
+    used = phedexData_.getSiteStorage(siteName)
+    cpu = popDbData_.getSiteCpu(siteName, date.strftime('%Y-%m-%d'))
+	fs = open('/local/cmsprod/IntelROCCS/DataDealer/Demo/DatasetData/System', 'a')
+	fs.write("%s\t%s\t%s\t%s\n" % (siteName, quota, used, cpu))
+	fs.close()
