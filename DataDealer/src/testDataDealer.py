b5870623ffbc0777587887b2819143b59f765d30
Bjorn Barrefors <barrefors@gmail.com>
2015-01-12 16:15:58 -0600

    testing everything

--- DataDealer/src/testDataDealer.py
+++ DataDealer/src/testDataDealer.py
@@ -25,24 +25,24 @@ config.read('/usr/local/IntelROCCS/DataDealer/intelroccs.test.cfg')
 # print data
 # print ""
 
-# # get all datasets
-# print " ----  Get Datasets  ---- "
-# phedexData = phedexData.phedexData()
-# datasets = phedexData.getAllDatasets()
-# print ""
+# get all datasets
+print " ----  Get Datasets  ---- "
+phedexData = phedexData.phedexData()
+datasets = phedexData.getAllDatasets()
+print ""
 
 # get all sites
 print " ----  Get Sites  ---- "
 sites_ = sites.sites()
 availableSites = sites_.getAvailableSites()
-print availableSites
 print ""
 
-# # rocker board algorithm
-# print " ----  Rocker Board Algorithm  ---- "
-# rba = rockerBoard.rockerBoard()
-# subscriptions = rba.rba(datasets, availableSites)
-# print ""
+# rocker board algorithm
+print " ----  Rocker Board Algorithm  ---- "
+rba = rockerBoard.rockerBoard()
+subscriptions = rba.rba(datasets, availableSites)
+print subscriptions
+print ""
 
 # # subscribe selected datasets
 # print " ----  Subscribe Datasets  ---- "
