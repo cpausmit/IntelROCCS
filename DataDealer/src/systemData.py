5c591be6c8bc3636a0cca65a8c754f9b9cd91548
Bjorn Barrefors <barrefors@gmail.com>
2015-01-15 16:19:38 -0600

    Added list comprehension

--- DataDealer/src/systemData.py
+++ DataDealer/src/systemData.py
@@ -17,7 +17,7 @@ allSites = sites_.getAllSites()
 blacklistedSites = sites_.getBlacklistedSites()
 today = datetime.date.today()
 today = datetime.datetime.combine(today, datetime.time(0,0,0,0))
-availableSites = allSites - blacklistedSites
+availableSites = [site for site in allSites if item not in blacklistedSites]
 for siteName in availableSites:
     subscriptions = []
     query = "SELECT Datasets.DatasetName FROM Requests INNER JOIN Datasets ON Datasets.DatasetId=Requests.DatasetId INNER JOIN Sites ON Sites.SiteId=Requests.SiteId WHERE Requests.Date>%s AND Sites.SiteName=%s AND Requests.RequestType=%s"
