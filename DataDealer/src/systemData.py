3b3cc52e99df6db38d488e6e5a35b1519efd0b48
Bjorn Barrefors <barrefors@gmail.com>
2015-01-15 16:17:55 -0600

    Dont show sites that are down

--- DataDealer/src/systemData.py
+++ DataDealer/src/systemData.py
@@ -17,7 +17,8 @@ allSites = sites_.getAllSites()
 blacklistedSites = sites_.getBlacklistedSites()
 today = datetime.date.today()
 today = datetime.datetime.combine(today, datetime.time(0,0,0,0))
-for siteName in allSites:
+availableSites = allSites - blacklistedSites
+for siteName in availableSites:
     subscriptions = []
     query = "SELECT Datasets.DatasetName FROM Requests INNER JOIN Datasets ON Datasets.DatasetId=Requests.DatasetId INNER JOIN Sites ON Sites.SiteId=Requests.SiteId WHERE Requests.Date>%s AND Sites.SiteName=%s AND Requests.RequestType=%s"
     values = [today, siteName, 0]
