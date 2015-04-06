ce1ba5ed3d280d4788f3cab7c1ed6d6b18b9fc25
Bjorn Barrefors <barrefors@gmail.com>
2015-04-06 00:21:01 -0500

    More in test script

--- DataDealer/src/testDataDealer.py
+++ DataDealer/src/testDataDealer.py
@@ -3,6 +3,7 @@
 # This is the main script of the DataDealer. See README.md for more information.
 #---------------------------------------------------------------------------------------------------
 import sys, datetime
+import sites, dailyRockerBoard, report, subscriptionProgress
 import phedexData
 
 # initialize
@@ -18,6 +19,43 @@ print " ----  Get Datasets  ---- "
 startTime = datetime.datetime.now()
 phedexData_ = phedexData.phedexData()
 datasets = phedexData_.getAllDatasets()
+totalTime = datetime.datetime.now() - startTime
+print " ----  " + str(totalTime.seconds) + "s " + str(totalTime.microseconds) + "ms" + "  ---- "
+print ""
+
+# get all sites
+print " ----  Get Sites  ---- "
+startTime = datetime.datetime.now()
+sites_ = sites.sites()
+availableSites = sites_.getAvailableSites()
+totalTime = datetime.datetime.now() - startTime
+print " ----  " + str(totalTime.seconds) + "s " + str(totalTime.microseconds) + "ms" + "  ---- "
+print ""
+
+# rocker board algorithm
+print " ----  Rocker Board Algorithm  ---- "
+startTime = datetime.datetime.now()
+dailyRockerBoard_ = dailyRockerBoard.dailyRockerBoard()
+subscriptions = dailyRockerBoard_.dailyRba(datasets, availableSites)
+totalTime = datetime.datetime.now() - startTime
+print " ----  " + str(totalTime.seconds) + "s " + str(totalTime.microseconds) + "ms" + "  ---- "
+print ""
+
+# update and check progress of subscriptions
+print " ----  Update and Check Subscriptions  ---- "
+startTime = datetime.datetime.now()
+subscriptionProgress_ = subscriptionProgress.subscriptionProgress()
+subscriptionProgress_.updateProgress()
+subscriptionProgress_.checkProgress()
+totalTime = datetime.datetime.now() - startTime
+print " ----  " + str(totalTime.seconds) + "s " + str(totalTime.microseconds) + "ms" + "  ---- "
+print ""
+
+# send summary report
+print " ----  Daily Summary  ---- "
+startTime = datetime.datetime.now()
+report_ = report.report()
+report_.createDailyReport()
 totalTime = datetime.datetime.now() - startTime
 print " ----  " + str(totalTime.seconds) + "s " + str(totalTime.microseconds) + "ms" + "  ---- "
 print ""
