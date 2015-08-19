3742827f792c1e9e9f7807f3fd450dcdd87e88e3
Christoph Paus <paus@mit.edu>
2015-08-19 13:34:00 -0400

    Adding all unknown files.

---
+++ Monitor/recover.sh
@@ -0,0 +1,49 @@
+#tar fzx /backup/complete/cmsprod/cmsprod080314.tgz \
+#      /export/home/cmsprod/IntelROCCS/Monitor/readPhedexCache.py \
+#      /export/home/cmsprod/IntelROCCS/Monitor/das_client.py \
+#      /export/home/cmsprod/IntelROCCS/Monitor/extractDataFromLogs.sh \
+#      /export/home/cmsprod/IntelROCCS/Monitor/monitord \
+#      /export/home/cmsprod/IntelROCCS/Monitor/sysv/monitord \
+#      /export/home/cmsprod/IntelROCCS/Monitor/findDatasetProperties.py \
+#      /export/home/cmsprod/IntelROCCS/Monitor/monitor.sh
+
+tar fzx /backup/incremental/cmsprod/cmsprod080414.tgz \
+      /export/home/cmsprod/IntelROCCS/Monitor/readPhedexCache.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/das_client.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/extractDataFromLogs.sh \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/sysv/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/findDatasetProperties.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitor.sh
+tar fzx /backup/incremental/cmsprod/cmsprod080514.tgz \
+      /export/home/cmsprod/IntelROCCS/Monitor/readPhedexCache.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/das_client.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/extractDataFromLogs.sh \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/sysv/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/findDatasetProperties.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitor.sh
+tar fzx /backup/incremental/cmsprod/cmsprod080614.tgz \
+      /export/home/cmsprod/IntelROCCS/Monitor/readPhedexCache.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/das_client.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/extractDataFromLogs.sh \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/sysv/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/findDatasetProperties.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitor.sh
+tar fzx /backup/incremental/cmsprod/cmsprod080714.tgz \
+      /export/home/cmsprod/IntelROCCS/Monitor/readPhedexCache.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/das_client.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/extractDataFromLogs.sh \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/sysv/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/findDatasetProperties.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitor.sh
+tar fzx /backup/incremental/cmsprod/cmsprod080814.tgz \
+      /export/home/cmsprod/IntelROCCS/Monitor/readPhedexCache.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/das_client.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/extractDataFromLogs.sh \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/sysv/monitord \
+      /export/home/cmsprod/IntelROCCS/Monitor/findDatasetProperties.py \
+      /export/home/cmsprod/IntelROCCS/Monitor/monitor.sh
